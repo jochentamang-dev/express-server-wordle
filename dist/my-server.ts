a26950e1f48436d676826e6c04d24b6ffae3c072
jochentamang-dev <tamangj@mail.gvsu.edu>
2023-04-13 17:29:15 -0400

    express server

---
+++ dist/my-server.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from "express"
+import cors from "cors"
+const app: Application = express()
+const PORT = process.env.PORT ?? 8000;
+
+app.use(cors())
+// getting endpoint
+app.get("/", (req: Request, res: Response) => {
+    res.send({ secret: get_word() })
+})
+
+function get_word() {
+    const secretWords = ["HELLO", "LOVER", "APPLE", "QUIET", "HANDS", "FEETS", "TABLE", "CHAIR", "BLURT", "SHOUT", "REACT", "MAJOR", "DEATH", "CRUST", "OUTDO", "ADOBE"]
+    return secretWords[Math.floor(Math.random() * (secretWords.length))];
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
